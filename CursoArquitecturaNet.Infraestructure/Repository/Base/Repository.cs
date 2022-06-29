f617d36e52a7a73b8d6c01fbc018a280577f1445
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-29 20:51:29 +0200

    Add class Repository<T> : IRepository<T> where T : BaseEntity

---
+++ CursoArquitecturaNet.Infraestructure/Repository/Base/Repository.cs
@@ -0,0 +1,52 @@
+﻿
+using CursoArquitecturaNet.Core.Entities.Base;
+using CursoArquitecturaNet.Core.Repositories.Base;
+using CursoArquitecturaNet.Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CursoArquitecturaNet.Infraestructure.Repository.Base
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly CursoArquitecturaNetContext dbContext;
+        public Repository(CursoArquitecturaNetContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            dbContext.Set<T>().Add(entity);
+            await dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            dbContext.Set<T>().Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await dbContext.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            dbContext.Entry(entity).State = EntityState.Modified;
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
