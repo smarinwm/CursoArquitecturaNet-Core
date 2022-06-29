25fc9f8b0f0af64bde5515f1c4b46150491edbd1
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-29 21:18:41 +0200

    Add IProductService

---
+++ CursoArquitecturaNet.Application/Interfaces/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using CursoArquitecturaNet.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoArquitecturaNet.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetProductList();
+        Task<Product> GetProductById(int productId);
+        Task<IEnumerable<Product>> GetProductByName(string product);
+        Task<Product> Create(Product product);
+        Task Update(Product product);
+        Task Delete(Product product);
+     }
+}
