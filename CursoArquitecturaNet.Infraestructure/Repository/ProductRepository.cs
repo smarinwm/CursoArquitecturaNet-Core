08897b12657e0dbe1ef4358c7d5f60fa349172d8
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-29 21:07:08 +0200

    Add class ProductRepository : Repository<Product>, IProductRepository

---
+++ CursoArquitecturaNet.Infraestructure/Repository/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using CursoArquitecturaNet.Core.Entities;
+using CursoArquitecturaNet.Core.Repositories;
+using CursoArquitecturaNet.Infraestructure.Data;
+using CursoArquitecturaNet.Infraestructure.Repository.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoArquitecturaNet.Infraestructure.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(CursoArquitecturaNetContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByNameAsync(string productName)
+        {
+            return await dbContext.Products.Where(x => x.ProductName.Contains(productName)).ToListAsync();
+        }
+
+    }
+}
