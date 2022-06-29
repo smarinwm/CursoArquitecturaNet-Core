acb26bf245b4a98340d5661d76b429b5de7354a0
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-29 19:22:34 +0200

    Add proyecto Net.Core & BaseEntity

---
+++ CursoArquitecturaNet.Core/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using CursoArquitecturaNet.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoArquitecturaNet.Core.Entities
+{
+    public class Product : BaseEntity
+    {
+    }
+}
