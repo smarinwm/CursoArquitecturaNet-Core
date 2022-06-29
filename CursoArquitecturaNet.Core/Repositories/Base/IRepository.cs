cb973540a9cd04b20b5f5cf0fa55a068981c6a8c
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-29 19:42:13 +0200

    Add Task - IRepository

--- CursoArquitecturaNet.Core/Repositories/Base/IRepository.cs
+++ CursoArquitecturaNet.Core/Repositories/Base/IRepository.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CursoArquitecturaNet.Core.Entities.Base;
+using System.Linq.Expressions;
 
 namespace CursoArquitecturaNet.Core.Repositories.Base
 {
-    public interface IRepository
+    public interface IRepository<T> where T : BaseEntity
     {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
     }
 }
