94cb21d8faf04c8837cc70aaff2ce3265209bfb3
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-29 19:48:16 +0200

    Add IProductRepository : IRepository<Product> & Task

---
+++ CursoArquitecturaNet.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using CursoArquitecturaNet.Core.Entities;
+using CursoArquitecturaNet.Core.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoArquitecturaNet.Core.Repositories
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        Task<IEnumerable<Product>> GetProductByNameAsync(string productName);
+    }
+}
