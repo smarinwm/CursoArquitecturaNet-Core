b0b563eda691f0e957d0310e414ecc5ed5614067
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-29 20:15:14 +0200

    Add DbSet<Product> Products { get; set; }

--- CursoArquitecturaNet.Infraestructure/Data/CursoArquitecturaNetContext.cs
+++ CursoArquitecturaNet.Infraestructure/Data/CursoArquitecturaNetContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CursoArquitecturaNet.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace CursoArquitecturaNet.Infraestructure.Data
 {
-    public class CursoArquitecturaNetContext
+    public class CursoArquitecturaNetContext : DbContext
     {
+        public CursoArquitecturaNetContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
     }
 }
