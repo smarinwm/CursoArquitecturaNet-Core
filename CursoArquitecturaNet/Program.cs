d74ffec1b2c59f0a256764cdc98b3fd9bc193ef8
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-30 11:55:37 +0200

    Add conf Program.cs

--- CursoArquitecturaNet/Program.cs
+++ CursoArquitecturaNet/Program.cs
@@ -1,3 +1,10 @@
+using CursoArquitecturaNet.Application.Interfaces;
+using CursoArquitecturaNet.Application.Services;
+using CursoArquitecturaNet.Core.Repositories;
+using CursoArquitecturaNet.Core.Repositories.Base;
+using CursoArquitecturaNet.Infraestructure.Repository;
+using CursoArquitecturaNet.Infraestructure.Repository.Base;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddScoped<IProductService, ProductsService>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
